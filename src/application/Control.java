56977b76d801ad47e88192c07feaa975d7ac9c76
Bohonalo <bohonalo@gmail.com>
2017-12-01 11:11:28 +0100

    1.3

--- src/application/Control.java
+++ src/application/Control.java
@@ -1,13 +1,30 @@
 package application;
 
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class Control {
 	
 	 @FXML
 	    void Abrir(MouseEvent event) {
-	    	System.out.println("pepe");
+		 try {
+	           FXMLLoader loader = new FXMLLoader(Main.class.getResource("../vista/FreeSolo.fxml"));
+	           AnchorPane ventanaDos = (AnchorPane) loader.load();
+	           Stage ventana = new Stage();
+	           Scene scene = new Scene(ventanaDos);
+	           ventana.setScene(scene);
+	           ventana.show();
+
+	       } catch (Exception e) {
+	           //tratar la excepción
+	       }
+		 System.out.println("pepe");
+			
 	    }
 
 }
