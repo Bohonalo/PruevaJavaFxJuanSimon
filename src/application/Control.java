e82ed51f502f546b00beb5d233915c9f7aed7588
Bohonalo <bohonalo@gmail.com>
2017-12-01 11:03:26 +0100

    1.2

---
+++ src/application/Control.java
@@ -0,0 +1,13 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class Control {
+	
+	 @FXML
+	    void Abrir(MouseEvent event) {
+	    	System.out.println("pepe");
+	    }
+
+}
