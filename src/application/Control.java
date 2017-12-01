368fe6c8dbf0db969e71844b9025ff49af7688b1
Bohonalo <bohonalo@gmail.com>
2017-12-01 11:28:10 +0100

    2.0

--- src/application/Control.java
+++ src/application/Control.java
@@ -1,29 +1,47 @@
 package application;
 
 
+import com.bohonalo.controles.ventana.OpcionesVentana;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Control {
+	
+	OpcionesVentana o = new OpcionesVentana();
+	
+    @FXML
+    private AnchorPane Anchor;
+
+    @FXML
+    void Salir(MouseEvent event) {
+    	System.exit(0);
+    }
+    @FXML
+    void Moverse(MouseEvent event) {
+    	o.moverVentanaAnchor(Anchor);
+    }
 	
 	 @FXML
 	    void Abrir(MouseEvent event) {
 		 try {
 	           FXMLLoader loader = new FXMLLoader(Main.class.getResource("../vista/FreeSolo.fxml"));
-	           AnchorPane ventanaDos = (AnchorPane) loader.load();
+	           AnchorPane ventanaDos = (AnchorPane) loader.load();           
 	           Stage ventana = new Stage();
+	           ventana.initStyle(StageStyle.TRANSPARENT);
 	           Scene scene = new Scene(ventanaDos);
 	           ventana.setScene(scene);
 	           ventana.show();
+	           
 
 	       } catch (Exception e) {
 	           //tratar la excepción
 	       }
-		 System.out.println("pepe");
 			
 	    }
 
